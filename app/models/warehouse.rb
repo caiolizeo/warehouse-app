4b56f566c1d44b7b4b4f06811d1d8f82ed044d89
caiolizeo <caio-lz@hotmail.com>
2021-12-13 16:45:16 -0300

    testes de model criados

--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -1,9 +1,9 @@
 class Warehouse < ApplicationRecord
-  validates :description, :address,
+  validates :name, :code, :description, :address, :postal_code,
             :city, :state, :total_area, :useful_area,
             presence: true
-  validates :name, :code, uniqueness: true, presence: true
-  validates :postal_code, format: { with: /\A\d{5}\-\d{3}\z/,
-                                    message: 'possui formato inválido' },
-                          presence: true
+  validates :name, :code, uniqueness: true
+  validates :postal_code, format: { with: /\d{5}\-\d{3}/,
+                                    message: 'possui formato inválido' }
+                          
 end
--- spec/models/warehouse_spec.rb
+++ spec/models/warehouse_spec.rb
@@ -1,4 +1,50 @@
 require "rails_helper"
 
 RSpec.describe Warehouse, type: :model do
+  it 'código duplicado' do
+
+    warehouse = Warehouse.create(name: 'Maceió', code: 'MCZ', description: 'Ótimo galpão',
+                               address: 'Av Fernandes Lima', city: 'Maceió',
+                               state: 'AL', postal_code: '57050-000', total_area: 10000, useful_area: 8000)
+
+    warehouse2 = Warehouse.new(name: 'Guarulhos', code: 'MCZ', description: 'Galpão em Guarulhos',
+                             address: 'Rua x', city: 'Guarulhos', state: 'SP', postal_code: '04200-000',
+                             total_area: 5000, useful_area: 3000)
+
+    result = warehouse2.valid?
+
+    expect(result).to eq false
+  end
+
+  context 'cep inválido não pode ser registrado' do
+    it 'cep sem - ' do
+      warehouse2 = Warehouse.new(name: 'Guarulhos', code: 'MCZ', description: 'Galpão em Guarulhos',
+                                address: 'Rua x', city: 'Guarulhos', state: 'SP', postal_code: '04200000',
+                                total_area: 5000, useful_area: 3000)
+
+      result = warehouse2.valid?
+
+      expect(result).to eq false
+    end
+
+    it 'cep com uma letra' do
+      warehouse2 = Warehouse.new(name: 'Guarulhos', code: 'MCZ', description: 'Galpão em Guarulhos',
+                                address: 'Rua x', city: 'Guarulhos', state: 'SP', postal_code: 'o0200000',
+                                total_area: 5000, useful_area: 3000)
+      
+      result = warehouse2.valid?
+
+      expect(result).to eq false
+    end
+
+    it 'cep com espaços' do
+      warehouse2 = Warehouse.new(name: 'Guarulhos', code: 'MCZ', description: 'Galpão em Guarulhos',
+                                address: 'Rua x', city: 'Guarulhos', state: 'SP', postal_code: '     -   ',
+                                total_area: 5000, useful_area: 3000)
+
+      result = warehouse2.valid?
+
+      expect(result).to eq false
+    end
+  end
 end
