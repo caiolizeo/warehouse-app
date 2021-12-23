b90332d175d279abb868af52bffb26daa5f0c3c9
Caio Lizeo <caiolizeo@outlook.com>
2021-12-22 21:29:54 -0300

    adiciona edição de produto

--- spec/system/edit_product_model_spec.rb
+++ spec/system/edit_product_model_spec.rb
@@ -10,16 +10,16 @@ describe 'Usuário edita um produto' do
                              email: 'contato@cconfec.com', phone: '99999-9000')
 
     p1 = ProductModel.create!(name: 'Caneca Marvel', height: '14', width: '10', length: '8',
-                         weight: 300, provider: prov1)
+                              weight: 300, provider: prov1)
     p2 = ProductModel.create!(name: 'Boneco Homem Aranha', height: '50', width: '30', length: '15',
-                         weight: 250, provider: prov1)
+                              weight: 250, provider: prov1)
     p3 = ProductModel.create!(name: 'Camiseta Homem de ferro', height: '70', width: '40', length: '1',
-                         weight: 100, provider: prov2)
+                              weight: 100, provider: prov2)
 
     visit root_path
     click_on 'Ver todos os produtos'
 
-    within('tr#2') do
+    within("tr##{p2.id}") do
       click_on 'Editar'
     end
 
@@ -38,7 +38,75 @@ describe 'Usuário edita um produto' do
     expect(page).to have_content('Fornecedor: C Modas')
   end
 
-  it 'deixa campos em branco' do
-    
+  it 'e deixa campos em branco' do
+    prov1 = Provider.create!(trading_name: 'A Presentes', company_name: 'A importações LTDA ME',
+                             cnpj: '08.385.207/0001-33', address: 'Av Paulista 500',
+                             email: 'contato@apresentes.com', phone: '99999-9999')
+    prov2 = Provider.create!(trading_name: 'C Modas', company_name: 'C Confecções LTDA',
+                             cnpj: '18.021.478/0001-63', address: 'Av Europa 250', 
+                             email: 'contato@cconfec.com', phone: '99999-9000')
+
+    p1 = ProductModel.create!(name: 'Caneca Marvel', height: '14', width: '10', length: '8',
+                              weight: 300, provider: prov1)
+    p2 = ProductModel.create!(name: 'Boneco Homem Aranha', height: '50', width: '30', length: '15',
+                              weight: 250, provider: prov1)
+    p3 = ProductModel.create!(name: 'Camiseta Homem de ferro', height: '70', width: '40', length: '1',
+                              weight: 100, provider: prov2)
+
+    visit root_path
+    click_on 'Ver todos os produtos'
+
+    within("tr##{p2.id}") do
+      click_on 'Editar'
+    end
+
+    fill_in 'Nome', with: ''
+    fill_in 'Peso', with: ''
+    fill_in 'Altura', with: ''
+    fill_in 'Largura', with: ''
+    fill_in 'Profundidade', with: ''
+    click_on 'Editar'
+
+    expect(page).to have_content('Não foi possível editar o produto')
+    expect(page).to have_content('Nome não pode ficar em branco')
+    expect(page).to have_content('Peso não pode ficar em branco')
+    expect(page).to have_content('Altura não pode ficar em branco')
+    expect(page).to have_content('Largura não pode ficar em branco')
+    expect(page).to have_content('Profundidade não pode ficar em branco')
+  end
+
+  it 'e utiliza valores inválidos para peso e dimensões' do
+    prov1 = Provider.create!(trading_name: 'A Presentes', company_name: 'A importações LTDA ME',
+                             cnpj: '08.385.207/0001-33', address: 'Av Paulista 500',
+                             email: 'contato@apresentes.com', phone: '99999-9999')
+    prov2 = Provider.create!(trading_name: 'C Modas', company_name: 'C Confecções LTDA',
+                             cnpj: '18.021.478/0001-63', address: 'Av Europa 250', 
+                             email: 'contato@cconfec.com', phone: '99999-9000')
+
+    p1 = ProductModel.create!(name: 'Caneca Marvel', height: '14', width: '10', length: '8',
+                              weight: 300, provider: prov1)
+    p2 = ProductModel.create!(name: 'Boneco Homem Aranha', height: '50', width: '30', length: '15',
+                              weight: 250, provider: prov1)
+    p3 = ProductModel.create!(name: 'Camiseta Homem de ferro', height: '70', width: '40', length: '1',
+                              weight: 100, provider: prov2)
+
+    visit root_path
+    click_on 'Ver todos os produtos'
+
+    within("tr##{p2.id}") do
+      click_on 'Editar'
+    end
+
+    fill_in 'Peso', with: '-5'
+    fill_in 'Altura', with: '0'
+    fill_in 'Largura', with: '-7'
+    fill_in 'Profundidade', with: '0'
+    click_on 'Editar'
+
+    expect(page).to have_content('Não foi possível editar o produto')
+    expect(page).to have_content('Peso deve ser maior que 0')
+    expect(page).to have_content('Altura deve ser maior que 0')
+    expect(page).to have_content('Largura deve ser maior que 0')
+    expect(page).to have_content('Profundidade deve ser maior que 0')
   end
 end
--- spec/system/visit_product_bundle_spec.rb
+++ spec/system/visit_product_bundle_spec.rb
@@ -6,17 +6,18 @@ describe 'Um usuário vê todos os bundles cadastrados' do
     p = Provider.create!(trading_name: 'A Presentes', company_name: 'A importações LTDA ME',
                          cnpj: '08.385.207/0001-33', address: 'Av Paulista 500',
                          email: 'contato@apresentes.com', phone: '99999-9999')
-    ProductModel.create!(name: 'Caneca Marvel', height: '14', width: '10', length: '8',
-                         weight: 300, provider: p)
-    ProductModel.create!(name: 'Boneco Homem Aranha', height: '50', width: '30', length: '15',
-                         weight: 250, provider: p)
-    ProductModel.create!(name: 'Camiseta Homem de ferro', height: '75', width: '40', length: '1',
-                         weight: 100, provider: p)
+                         
+    product1 = ProductModel.create!(name: 'Caneca Marvel', height: '14', width: '10', length: '8',
+                                    weight: 300, provider: p)
+    product2 = ProductModel.create!(name: 'Boneco Homem Aranha', height: '50', width: '30', length: '15',
+                                    weight: 250, provider: p)
+    product3 = ProductModel.create!(name: 'Camiseta Homem de ferro', height: '75', width: '40', length: '1',
+                                    weight: 100, provider: p)
 
     ProductBundle.create!(name: 'Kit Nerd', sku: 'D5E9D5E8R7S5D6T9H5Y4', 
-                          product_model_ids: ['1', '2', '3'])
+                          product_models: [product1, product2, product3])
     ProductBundle.create!(name: 'Kit camiseta e caneca', sku: 'F5G6R9S5F4T7S58T9K62', 
-                          product_model_ids: ['1', '3'])
+                          product_models: [product1, product3])
 
     visit root_path
     click_on 'Ver todos os kits'
