6b61927f387148ad9bba4b9f1eac1b1c7f0962dd
Caio Lizeo <caiolizeo@outlook.com>
2021-12-22 22:52:43 -0300

    WIP: adiciona testes unitários

--- spec/models/provider_spec.rb
+++ spec/models/provider_spec.rb
@@ -1,5 +1,61 @@
 require 'rails_helper'
 
 RSpec.describe Provider, type: :model do
- 
+  context 'atributos em branco' do
+    it 'Nome fantasia em branco' do
+      p = Provider.new(trading_name: '', company_name: 'C Confecções LTDA',
+          						 cnpj: '22.281.398/0001-14', address: 'Av Europa 250', 
+                       email: 'contato@cconfec.com', phone: '99999-9000')
+			
+			result = p.valid?
+
+			expect(result).to eq false
+     end
+
+		it 'Razão social em branco' do
+			p = Provider.new(trading_name: 'C Modas', company_name: '',
+											 cnpj: '22.281.398/0001-14', address: 'Av Europa 250', 
+											 email: 'contato@cconfec.com', phone: '99999-9000')
+
+			result = p.valid?
+
+			expect(result).to eq false
+		end
+
+		it 'cnpj em branco' do
+			p = Provider.new(trading_name: 'C Modas', company_name: 'C Confecções LTDA',
+										 	 cnpj: '', address: 'Av Europa 250', 
+											 email: 'contato@cconfec.com', phone: '99999-9000')
+
+			result = p.valid?
+
+			expect(result).to eq false
+		end
+
+
+
+		it 'email em branco' do
+			p = Provider.new(trading_name: 'C Modas', company_name: 'C Confecções LTDA',
+										 	 cnpj: '22.281.398/0001-14', address: 'Av Europa 250', 
+											 email: '', phone: '99999-9000')
+
+			result = p.valid?
+
+			expect(result).to eq false
+		end
+  end
+
+	
+	it 'cnpj com fortmato inválido' do
+		p = Provider.new(trading_name: 'C Modas', company_name: 'C Confecções LTDA',
+											cnpj: '22281398000114', address: 'Av Europa 250', 
+											email: 'contato@cconfec.com', phone: '99999-9999')
+
+		result = p.valid?
+
+		expect(result).to eq false
+	end
+
+
+	
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  it 'email duplicado' do
+    user1 = User.create(email: 'email@teste.com', password: '123456789')
+    user2 = User.create(email: 'email@teste.com', password: 'abcdefghi')
+
+    result = user2.valid?
+
+    expect(result).to eq false
+  end
   
+  it 'senha menor que 6 dígitos' do
+    user1 = User.create(email: 'email@teste.com', password: '12345')
+
+    result = user1.valid?
+
+    expect(result).to eq false
+  end
+
+  context 'email com formato inválido' do
+    it 'email sem domínio' do
+      user1 = User.new(email: 'email@', password: '123456789')
+      
+      result = user1.valid?
+
+      expect(result).to eq false
+    end
+
+    it 'email sem @' do
+      user1 = User.new(email: 'emailteste.com', password: '123456789')
+
+      result = user1.valid?
+
+      expect(result).to eq false
+    end
+
+    it 'email sem nome do usuario' do
+      user1 = User.new(email: '@teste.com', password: '123456789')
+
+      result = user1.valid?
+
+      expect(result).to eq false
+    end
+  end
 end
